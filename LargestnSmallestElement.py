d52420e032e958acdebe7ca4eaa4f0bc4ed1588a
AryaaPatil <aarupatil2003@gmail.com>
2026-04-19 23:32:00 +0530

    Create LargestnSmallestElement.py

---
+++ LargestnSmallestElement.py
@@ -0,0 +1,17 @@
+"""Problem: Given a list of integers, find and return the largest element and smallest element 
+Approach: Iterate through the list and keep track of maximum and minimum element. """
+
+def largestnsmallest(l1):
+    max = float("-inf")
+    min = float("inf")
+    for i in range(len(l1)):
+        if max < l1[i]:
+            max = l1[i]
+        elif min > l1[i]:
+            min = l1[i]
+    print("Largest Element : ", max)
+    print("Smallest Element : ", min)
+            
+l1 = list(map(int, input().split()))
+largestnsmallest(l1)
+
